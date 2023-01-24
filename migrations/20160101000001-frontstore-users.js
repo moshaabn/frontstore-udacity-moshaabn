ab55bbeb1f631862ffb94e946e49feca80a2336c
Mohamed Shaaban <16676467+moshaabn@users.noreply.github.com>
2023-01-24 11:26:13 +0200

    Update 20160101000001-frontstore-users.js

--- migrations/20160101000001-frontstore-users.js
+++ migrations/20160101000001-frontstore-users.js
@@ -15,7 +15,7 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = function (db) {
-    var filePath = path.join(__dirname, 'sqls', '20160101000001-frontstore-users-up.sql');
+    var filePath = path.join(__dirname, 'sqls', '20160101000003-frontstore-users-up.sql');
     return new Promise(function (resolve, reject) {
         fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
             if (err) return reject(err);
@@ -29,7 +29,7 @@ exports.up = function (db) {
 };
 
 exports.down = function (db) {
-    var filePath = path.join(__dirname, 'sqls', '20160101000000-frontstore-down.sql');
+    var filePath = path.join(__dirname, 'sqls', '20160101000009-frontstore-users-down.sql');
     return new Promise(function (resolve, reject) {
         fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
             if (err) return reject(err);
